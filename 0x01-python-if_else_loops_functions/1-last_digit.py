20750b2cbf53acd204f585d81aaac0f7b25538fa
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2023-09-05 16:47:07 +0100

    fix error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 
-last_digit = (number) % 10
+last_digit = abs(number) % 10
 greater = "and is greater than 5"
 middle = "and is 0"
 lesser = "and is less than 6 and not 0"
