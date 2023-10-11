6202f79d146e8260193dc89f2492cf138089d9cd
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2023-10-11 16:53:27 +0100

    pycodestyle validation

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-""" is_same_class checks if the objest is an instance of the specified class """
+""" is_same_class checks if the objest is an instance of the 
+specified class """
+
 
 def is_same_class(obj, a_class):
-	"""return true if obj is the exact class a_class, otherwise false"""
-	return(type(obj) == a_class)
+    """return true if obj is the exact class a_class, otherwise false"""
+    return (type(obj) is a_class)
